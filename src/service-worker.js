8f750b075793226764832e3fdd38d30626e4a2a9
TristanG <tristan@gcans.com>
2024-01-31 01:19:24 -0500

    remove vite-pwa, use basic setup

---
+++ src/service-worker.js
@@ -0,0 +1,14 @@
+import { build, files, prerendered, version } from '$service-worker';
+import { precacheAndRoute } from 'workbox-precaching';
+
+const precache_list = [
+	'/', // Attention: serves stale index, might not be ideal for your use case.
+	...build,
+	...files,
+	...prerendered
+].map((s) => ({
+	url: s,
+	revision: version
+}));
+
+precacheAndRoute(precache_list);
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import adapter from '@sveltejs/adapter-auto';
-import { VitePWA } from 'vite-plugin-pwa';
 import * as dotenv from 'dotenv';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,40 +8,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	},
-	vite: {
-		plugins: [
-			VitePWA({
-				registerType: 'autoUpdate',
-				// includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
-				manifest: {
-					// Manifest configuration
-					name: 'Play Engine',
-					start_url: '/',
-					display: 'standalone',
-					share_target: {
-						action: '/share-target',
-						method: 'POST',
-						enctype: 'multipart/form-data',
-						params: {
-							title: 'name',
-							text: 'description',
-							url: 'link'
-						}
-					},
-					background_color: '#ffffff',
-					description: 'A description of your app.'
-					//   icons: [
-					// 	{
-					// 	  src: 'path/to/your/icon.png',
-					// 	  sizes: '192x192',
-					// 	  type: 'image/png'
-					// 	},
-					// 	// Other icons
-					//   ]
-				}
-			})
-		]
 	}
 };
 
