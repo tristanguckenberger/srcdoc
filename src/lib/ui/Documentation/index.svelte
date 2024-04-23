a83affc97f8a53a995a5a73a5a797981cbab3e73
TristanG <tristan@gcans.com>
2024-04-23 16:59:00 -0400

    check in

---
+++ src/lib/ui/Documentation/index.svelte
@@ -0,0 +1,95 @@
+<script>
+	// @ts-nocheck
+	/**
+	 * @name Documentation
+	 * @description This is a documentation component, it is used to display documentation for the application
+	 * and is primarily embedded in the editor, but can be used in other parts of the application.
+	 *
+	 *
+	 * It needs to be able to:
+	 * - Display documentation
+	 * - Display code snippets
+	 * - Display images
+	 * - Display videos *eventually*
+	 * - Display links
+	 * - Have a dropdown at the top to select the documentation to display
+	 * - Have previous and next links to navigate through the documentation, placed at the bottom of the page
+	 * - Be self-contained, meaning it should be able to be used in any part of the application
+	 *
+	 * Add Basic editor documentation in a new js file that exports a json object with the following structure:
+	 * [
+	 *     {
+	 *        "title": "Title of the documentation",
+	 *       "content": "Content of the documentation",
+	 *     },
+	 *     {...},
+	 *     {...}
+	 * ]
+	 *
+	 */
+
+	import { tick } from 'svelte';
+
+	let docsSelection;
+	let selectedOption = 0;
+	let docs = [
+		{
+			title: 'Getting Started',
+			content: 'Getting started content goes here'
+		},
+		{
+			title: 'Basic Concepts',
+			content: 'Basic concepts content goes here'
+		},
+		{
+			title: 'Advanced Concepts',
+			content: 'Advanced concepts content goes here'
+		}
+	];
+
+	async function handleDocPageChange(event) {
+		await tick();
+		selectedOption = event.target.value;
+	}
+
+	async function navigate(direction) {
+		let selectedCopy = parseInt(selectedOption);
+
+		direction === 'next' ? (selectedCopy += 1) : (selectedCopy -= 1);
+		selectedOption = selectedCopy;
+		await tick();
+		docsSelection.value = selectedOption;
+	}
+
+	$: canNavigateNext = selectedOption < docs.length - 1;
+	$: canNavigatePrevious = selectedOption > 0;
+</script>
+
+<div class="documentation-container">
+	<select
+		name="docs-selection"
+		id="docs-selection"
+		on:change={handleDocPageChange}
+		bind:this={docsSelection}
+	>
+		{#each docs as doc, i}
+			<option value={i}>{doc?.title}</option>
+		{/each}
+	</select>
+
+	<div class="documentation-content">
+		<h1>{docs[selectedOption]?.title}</h1>
+		<p>{docs[selectedOption]?.content}</p>
+	</div>
+
+	<div class="doc-navigation-container">
+		<button disabled={!canNavigatePrevious} on:click={() => navigate('prev')}>Previous</button>
+		<button disabled={!canNavigateNext} on:click={() => navigate('next')}>Next</button>
+	</div>
+</div>
+
+<style>
+	.documentation-content {
+		color: var(--color-primary);
+	}
+</style>
--- src/routes/games/[slug]/engine/+page.svelte
+++ src/routes/games/[slug]/engine/+page.svelte
@@ -46,9 +46,13 @@
 	import { routeHistoryStore } from '$lib/stores/routeStore.js';
 	import { gameSession } from '$lib/stores/gameSession/index.js';
 	import { modalFullInfoStore, editorPageInfoStore } from '$lib/stores/InfoStore.js';
+	import Documentation from '$lib/ui/Documentation/index.svelte';
+	import ToolTip from '$lib/ui/ToolTip/index.svelte';
 
 	export let data;
 	const play = false;
+	let docsOpen = false;
+	let showToolTip = false;
 
 	$: if (data) {
 		// this will be our data's starting point
@@ -123,8 +127,6 @@
 		fileSystemSidebarOpen.set(true);
 		codePanes2.set([]);
 	});
-
-	// $: console.log('engine::routeHistoryStore::', $routeHistoryStore);
 </script>
 
 <svelte:head>
@@ -134,35 +136,76 @@
 <div class="main" class:isSideBarOpen class:isFileSystemSideBarOpen>
 	<SplitPane
 		panes={['#split-file-explorer', '#split-input-output']}
-		sizes={[20, 80]}
+		sizes={[50, 50]}
 		vertical={false}
 		bind:this={$editorElement}
 	>
 		<div
 			id="split-file-explorer"
 			class:hidden={!$fileSystemSidebarOpen}
+			class:docsOpen
 			bind:clientWidth={$fileSystemSidebarWidth}
 		>
-			<a href={previousRoute} class="back">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="32"
-					height="32"
-					fill="#000000"
-					viewBox="0 0 256 256"
-					><path
-						d="M165.66,202.34a8,8,0,0,1-11.32,11.32l-80-80a8,8,0,0,1,0-11.32l80-80a8,8,0,0,1,11.32,11.32L91.31,128Z"
-					/></svg
+			<div class="action-row">
+				<a href={previousRoute} class="docs-button">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="32"
+						height="32"
+						fill="#000000"
+						viewBox="0 0 256 256"
+						><path
+							d="M165.66,202.34a8,8,0,0,1-11.32,11.32l-80-80a8,8,0,0,1,0-11.32l80-80a8,8,0,0,1,11.32,11.32L91.31,128Z"
+						/></svg
+					>
+				</a>
+				<button
+					class="docs-button"
+					on:click={() => {
+						// fileSystemSidebarOpen.set(false);
+						docsOpen = !docsOpen;
+
+						console.log('docsOpen::', docsOpen);
+					}}
 				>
-				Back
-			</a>
+					<div
+						class="icon-tooltip-container"
+						on:mouseover={() => {
+							showToolTip = true;
+						}}
+						on:mouseleave={() => {
+							showToolTip = false;
+						}}
+						on:focus={() => {}}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="32"
+							height="32"
+							fill="#000000"
+							viewBox="0 0 256 256"
+							><path
+								d="M213.66,66.34l-40-40A8,8,0,0,0,168,24H88A16,16,0,0,0,72,40V56H56A16,16,0,0,0,40,72V216a16,16,0,0,0,16,16H168a16,16,0,0,0,16-16V200h16a16,16,0,0,0,16-16V72A8,8,0,0,0,213.66,66.34ZM168,216H56V72h76.69L168,107.31v84.53c0,.06,0,.11,0,.16s0,.1,0,.16V216Zm32-32H184V104a8,8,0,0,0-2.34-5.66l-40-40A8,8,0,0,0,136,56H88V40h76.69L200,75.31Zm-56-32a8,8,0,0,1-8,8H88a8,8,0,0,1,0-16h48A8,8,0,0,1,144,152Zm0,32a8,8,0,0,1-8,8H88a8,8,0,0,1,0-16h48A8,8,0,0,1,144,184Z"
+							/></svg
+						>
+					</div></button
+				>
+				{#if showToolTip}
+					<ToolTip text="Open/Close Editor Docs" position="bottom" />
+				{/if}
+			</div>
+
 			<hr class="sidebar-divider" />
-			<FileTree
-				files={data?.files}
-				parentFileId={null}
-				gameId={data?.id}
-				userId={data?.userId ?? data?.user_id}
-			/>
+			{#if !docsOpen}
+				<FileTree
+					files={data?.files}
+					parentFileId={null}
+					gameId={data?.id}
+					userId={data?.userId ?? data?.user_id}
+				/>
+			{:else}
+				<Documentation />
+			{/if}
 		</div>
 		<div
 			bind:clientWidth={$inputOutputContainerWidth}
@@ -171,6 +214,7 @@
 			class:fullwidth={!$fileSystemSidebarOpen}
 			class:isSideBarOpen
 			class:isFileSystemSideBarOpen
+			class:docsOpen
 			style="--file-system-sidebar-width: {isFileSystemSideBarOpen
 				? $fileSystemSidebarWidth + 18
 				: 0}px; --sidebar-width: {isSideBarOpen ? 240 : 0}px;"
@@ -178,7 +222,7 @@
 			<SplitPane
 				panes={$openFiles?.length > 0 ? ['#split-2', '#split-3'] : ['#split-3']}
 				sizes={$openFiles?.length > 0 ? [50, 50] : [100]}
-				vertical={value}
+				vertical={true}
 				bind:this={$editorElement}
 			>
 				<!-- Editor Content -->
@@ -258,6 +302,10 @@
 		max-width: 245px;
 	}
 
+	#split-file-explorer.docsOpen {
+		max-width: 500px !important;
+	}
+
 	@media (min-width: 498px) {
 		/* #split-file-explorer {
 			height: calc(100% + 16px);
@@ -302,6 +350,10 @@
 		max-height: unset !important;
 		max-height: calc(100%);
 	}
+	#split-input-output.isFileSystemSideBarOpen.docsOpen {
+		max-width: calc(100%);
+		min-width: calc(100% - 30%);
+		/* max-width: calc(100% - var(--sidebar-width));
 	.sidebar-divider {
 		border: unset;
 		border-radius: 2px;
@@ -347,5 +399,76 @@
 	a.back svg {
 		width: 23px;
 		fill: var(--color-primary);
+	}
+	button.docs-button {
+		background-color: var(--button-highlight) !important;
+		width: 36.5px !important;
+		height: 36.5px !important;
+		max-width: 36.5px !important;
+		max-height: 36.5px !important;
+		min-width: 36.5px !important;
+		min-height: 36.5px !important;
+		padding-block: 0;
+		color: var(--color-primary) !important;
+		border-width: 0;
+		border-radius: 4px;
+		padding: 10px;
+		text-decoration: none;
+		font-size: 1rem;
+		font-family: var(--action-font) !important;
+		text-wrap: nowrap;
+		border-style: none !important;
+		display: flex;
+		align-items: center;
+		max-height: 36.5px;
+		height: 16.5px;
+		font-weight: 500;
+	}
+	a.docs-button svg,
+	button.docs-button svg {
+		width: 23px;
+		height: 23px;
+		fill: var(--color-primary);
+	}
+	a.docs-button {
+		background-color: var(--button-highlight) !important;
+		width: 36.5px !important;
+		height: 36.5px !important;
+		max-width: 36.5px !important;
+		max-height: 36.5px !important;
+		min-width: 36.5px !important;
+		min-height: 36.5px !important;
+		padding-block: 0;
+		color: var(--color-primary) !important;
+		border-width: 0;
+		border-radius: 4px;
+		text-decoration: none;
+		font-size: 1rem;
+		font-family: var(--action-font) !important;
+		text-wrap: nowrap;
+		border-style: none !important;
+		display: flex;
+		align-items: center;
+		max-height: 36.5px;
+		height: 16.5px;
+		font-weight: 500;
+		justify-content: center;
+	}
+	div.action-row {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		gap: 10px;
+	}
+	.icon-tooltip-container {
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		z-index: 1000000000000;
+	}
+	.action-row :global(.tooltip__container.bottom) {
+		top: 51px !important;
+		bottom: unset;
 	}
 </style>
